b78c2a66e7ba080be42d711b0177fcef3427620a
913165 <tinumistry@gmail.com>
2025-01-05 16:43:03 +0530

    included import in init py

--- app/services/__init__.py
+++ app/services/__init__.py
@@ -1,5 +1,6 @@
 # app/services/__init__.py
 from .vector_store_service import VectorStoreService
 from .payload_service import PayloadService
+from .search_service import SearchService
 
 # Add more service imports as needed
